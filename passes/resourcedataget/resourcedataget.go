806905534a88eb84f0c9d193cf30433302f9f01e
Brian Flad <bflad417@gmail.com>
2020-01-30 10:43:58 -0500

    New Analyzer: resourcedataget: returns ResourceData.Get() calls for later passes (#78)
    Reference: https://github.com/bflad/tfproviderlint/issues/74

---
+++ passes/resourcedataget/resourcedataget.go
@@ -0,0 +1,41 @@
+package resourcedataget
+
+import (
+	"go/ast"
+	"reflect"
+
+	"github.com/bflad/tfproviderlint/helper/terraformtype"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "resourcedataget",
+	Doc:  "find github.com/hashicorp/terraform-plugin-sdk/helper/schema.ResourceData.Get() calls for later passes",
+	Requires: []*analysis.Analyzer{
+		inspect.Analyzer,
+	},
+	Run:        run,
+	ResultType: reflect.TypeOf([]*ast.CallExpr{}),
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	nodeFilter := []ast.Node{
+		(*ast.CallExpr)(nil),
+	}
+	var result []*ast.CallExpr
+
+	inspect.Preorder(nodeFilter, func(n ast.Node) {
+		callExpr := n.(*ast.CallExpr)
+
+		if !terraformtype.IsHelperSchemaReceiverMethod(callExpr.Fun, pass.TypesInfo, terraformtype.TypeNameResourceData, "Get") {
+			return
+		}
+
+		result = append(result, callExpr)
+	})
+
+	return result, nil
+}
