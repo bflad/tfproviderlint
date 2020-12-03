883c8b463ebc064f0a90620af4ab84b6f181fdd0
Ryan King <rking@chanzuckerberg.com>
2020-12-03 11:57:02 -0800

    [fix] AT009 detects wrong usage
    AT009 would complain about
    
    ```go
    acctest.RandStringFromCharSet(1, acctest.CharSetAlpha)
    ```
    
    but should complain about
    
    ```go
    acctest.RandStringFromCharSet(1, acctest.CharSetAlphaNum)
    ```
    
    since the latter form is equivalent to `acctest.RandString(1)`
    
    Fixes #213

--- passes/AT009/testdata/src/a/main.go
+++ passes/AT009/testdata/src/a/main.go
@@ -16,11 +16,10 @@ func f() {
 
 	// Failing
 
-	_ = acctest.RandStringFromCharSet(1, acctest.CharSetAlpha) // want "should use RandString call instead"
+	_ = acctest.RandStringFromCharSet(1, acctest.CharSetAlphaNum) // want "should use RandString call instead"
 
 	// Passing
-
-	_ = acctest.RandStringFromCharSet(1, acctest.CharSetAlphaNum)
+	_ = acctest.RandStringFromCharSet(1, acctest.CharSetAlpha)
 	_ = acctest.RandStringFromCharSet(1, "abc123")
 	_ = acctest.RandString(1)
 }
