8ac5a839d6e66fcb29e572056fb30073c2a9a018
Brian Flad <bflad417@gmail.com>
2023-04-13 14:19:31 -0400

    deps: Update golang.org/x/tools@v0.8.0
    Reference: https://github.com/bflad/tfproviderlint/issues/255
    Reference: https://github.com/golang/go/issues/56494
    
    Manual upgrade due to prior Go panic:
    
    ```console
    $ go get golang.org/x/tools@v0.8.0
    panic: internal error: can't find reason for requirement on golang.org/x/crypto@v0.0.0-20200622213623-75b288015ac9
    
    goroutine 1 [running]:
    cmd/go/internal/modget.(*resolver).updateBuildList.func1({{0x1400043ed80, 0x13}, {0x140000c2e70, 0x22}})
            /opt/homebrew/Cellar/go/1.20.3/libexec/src/cmd/go/internal/modget/get.go:1760 +0xc4
    cmd/go/internal/modget.(*resolver).updateBuildList(0x1400007e000, {0x100f55398, 0x1400009e000}, {0x0, 0x0, 0x0})
            /opt/homebrew/Cellar/go/1.20.3/libexec/src/cmd/go/internal/modget/get.go:1765 +0x41c
    cmd/go/internal/modget.(*resolver).resolveQueries(0x1400007e000, {0x100f55398, 0x1400009e000}, {0x140000b0090, 0x1, 0x10126bde0?})
            /opt/homebrew/Cellar/go/1.20.3/libexec/src/cmd/go/internal/modget/get.go:1243 +0x148
    cmd/go/internal/modget.runGet({0x100f55398, 0x1400009e000}, 0x140000b8600?, {0x1400009a1a0, 0x1, 0x1})
            /opt/homebrew/Cellar/go/1.20.3/libexec/src/cmd/go/internal/modget/get.go:314 +0x38c
    main.invoke(0x10125e3c0, {0x1400009a190, 0x2, 0x2})
            /opt/homebrew/Cellar/go/1.20.3/libexec/src/cmd/go/main.go:225 +0x37c
    main.main()
            /opt/homebrew/Cellar/go/1.20.3/libexec/src/cmd/go/main.go:179 +0x7cc
    ```
    
    Updated via:
    
    ```shell
    go get golang.org/x/crypto@latest # panic workaround
    go get golang.org/x/tools@v0.8.0
    go mod tidy
    go mod vendor
    ```
    
    Introduces `xpasses/XR007` and `xpasses/XR008` test skips due to `golang.org/x/tools` now properly returning an error with invalid source files. This is expected with the suggested fixes implementation since those will intentionally remove source code, leaving the source code in an unbuildable state to encourage the developer to figure out the rest of the necessary solution.

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/bflad/gopaniccheck v0.1.0
 	github.com/hashicorp/terraform-plugin-sdk v1.16.1
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.5.0
-	golang.org/x/tools v0.0.0-20201028111035-eafbe7b904eb
+	golang.org/x/tools v0.8.0
 )
 
 require (
@@ -70,14 +70,13 @@ require (
 	github.com/zclconf/go-cty v1.7.1 // indirect
 	github.com/zclconf/go-cty-yaml v1.0.2 // indirect
 	go.opencensus.io v0.22.4 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
-	golang.org/x/mod v0.3.0 // indirect
-	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
-	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
-	golang.org/x/text v0.3.3 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/api v0.34.0 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/genproto v0.0.0-20200904004341-0bd0a958aa1d // indirect
--- vendor/golang.org/x/tools/internal/testenv/testenv.go
+++ vendor/golang.org/x/tools/internal/testenv/testenv.go
@@ -12,28 +12,33 @@ import (
 	"go/build"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"runtime"
+	"runtime/debug"
 	"strings"
 	"sync"
+	"testing"
+	"time"
+
+	"golang.org/x/tools/internal/goroot"
+
+	exec "golang.org/x/sys/execabs"
 )
-
-// Testing is an abstraction of a *testing.T.
-type Testing interface {
-	Skipf(format string, args ...interface{})
-	Fatalf(format string, args ...interface{})
-}
-
-type helperer interface {
-	Helper()
-}
 
 // packageMainIsDevel reports whether the module containing package main
 // is a development version (if module information is available).
-//
-// Builds in GOPATH mode and builds that lack module information are assumed to
-// be development versions.
-var packageMainIsDevel = func() bool { return true }
+func packageMainIsDevel() bool {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		// Most test binaries currently lack build info, but this should become more
+		// permissive once https://golang.org/issue/33976 is fixed.
+		return true
+	}
+
+	// Note: info.Main.Version describes the version of the module containing
+	// package main, not the version of “the main module”.
+	// See https://golang.org/issue/33975.
+	return info.Main.Version == "(devel)"
+}
 
 var checkGoGoroot struct {
 	once sync.Once
@@ -85,6 +90,23 @@ func hasTool(tool string) error {
 			GOROOT := strings.TrimSpace(string(out))
 			if GOROOT != runtime.GOROOT() {
 				checkGoGoroot.err = fmt.Errorf("'go env GOROOT' does not match runtime.GOROOT:\n\tgo env: %s\n\tGOROOT: %s", GOROOT, runtime.GOROOT())
+				return
+			}
+
+			// Also ensure that that GOROOT includes a compiler: 'go' commands
+			// don't in general work without it, and some builders
+			// (such as android-amd64-emu) seem to lack it in the test environment.
+			cmd := exec.Command(tool, "tool", "-n", "compile")
+			stderr := new(bytes.Buffer)
+			stderr.Write([]byte("\n"))
+			cmd.Stderr = stderr
+			out, err = cmd.Output()
+			if err != nil {
+				checkGoGoroot.err = fmt.Errorf("%v: %v%s", cmd, err, stderr)
+				return
+			}
+			if _, err := os.Stat(string(bytes.TrimSpace(out))); err != nil {
+				checkGoGoroot.err = err
 			}
 		})
 		if checkGoGoroot.err != nil {
@@ -160,14 +182,13 @@ func allowMissingTool(tool string) bool {
 
 // NeedsTool skips t if the named tool is not present in the path.
 // As a special case, "cgo" means "go" is present and can compile cgo programs.
-func NeedsTool(t Testing, tool string) {
-	if t, ok := t.(helperer); ok {
-		t.Helper()
-	}
+func NeedsTool(t testing.TB, tool string) {
 	err := hasTool(tool)
 	if err == nil {
 		return
 	}
+
+	t.Helper()
 	if allowMissingTool(tool) {
 		t.Skipf("skipping because %s tool not available: %v", tool, err)
 	} else {
@@ -177,10 +198,8 @@ func NeedsTool(t Testing, tool string) {
 
 // NeedsGoPackages skips t if the go/packages driver (or 'go' tool) implied by
 // the current process environment is not present in the path.
-func NeedsGoPackages(t Testing) {
-	if t, ok := t.(helperer); ok {
-		t.Helper()
-	}
+func NeedsGoPackages(t testing.TB) {
+	t.Helper()
 
 	tool := os.Getenv("GOPACKAGESDRIVER")
 	switch tool {
@@ -200,10 +219,8 @@ func NeedsGoPackages(t Testing) {
 
 // NeedsGoPackagesEnv skips t if the go/packages driver (or 'go' tool) implied
 // by env is not present in the path.
-func NeedsGoPackagesEnv(t Testing, env []string) {
-	if t, ok := t.(helperer); ok {
-		t.Helper()
-	}
+func NeedsGoPackagesEnv(t testing.TB, env []string) {
+	t.Helper()
 
 	for _, v := range env {
 		if strings.HasPrefix(v, "GOPACKAGESDRIVER=") {
@@ -220,24 +237,21 @@ func NeedsGoPackagesEnv(t Testing, env []string) {
 	NeedsGoPackages(t)
 }
 
-// NeedsGoBuild skips t if the current system can't build programs with ``go build''
+// NeedsGoBuild skips t if the current system can't build programs with “go build”
 // and then run them with os.StartProcess or exec.Command.
-// android, and darwin/arm systems don't have the userspace go build needs to run,
+// Android doesn't have the userspace go build needs to run,
 // and js/wasm doesn't support running subprocesses.
-func NeedsGoBuild(t Testing) {
-	if t, ok := t.(helperer); ok {
-		t.Helper()
-	}
+func NeedsGoBuild(t testing.TB) {
+	t.Helper()
+
+	// This logic was derived from internal/testing.HasGoBuild and
+	// may need to be updated as that function evolves.
 
 	NeedsTool(t, "go")
 
 	switch runtime.GOOS {
 	case "android", "js":
 		t.Skipf("skipping test: %v can't build and run Go binaries", runtime.GOOS)
-	case "darwin":
-		if strings.HasPrefix(runtime.GOARCH, "arm") {
-			t.Skipf("skipping test: darwin/arm can't build and run Go binaries")
-		}
 	}
 }
 
@@ -246,14 +260,31 @@ func NeedsGoBuild(t Testing) {
 //
 // It should be called from within a TestMain function.
 func ExitIfSmallMachine() {
-	switch os.Getenv("GO_BUILDER_NAME") {
-	case "linux-arm":
-		fmt.Fprintln(os.Stderr, "skipping test: linux-arm builder lacks sufficient memory (https://golang.org/issue/32834)")
-		os.Exit(0)
+	switch b := os.Getenv("GO_BUILDER_NAME"); b {
+	case "linux-arm-scaleway":
+		// "linux-arm" was renamed to "linux-arm-scaleway" in CL 303230.
+		fmt.Fprintln(os.Stderr, "skipping test: linux-arm-scaleway builder lacks sufficient memory (https://golang.org/issue/32834)")
 	case "plan9-arm":
 		fmt.Fprintln(os.Stderr, "skipping test: plan9-arm builder lacks sufficient memory (https://golang.org/issue/38772)")
-		os.Exit(0)
+	case "netbsd-arm-bsiegert", "netbsd-arm64-bsiegert":
+		// As of 2021-06-02, these builders are running with GO_TEST_TIMEOUT_SCALE=10,
+		// and there is only one of each. We shouldn't waste those scarce resources
+		// running very slow tests.
+		fmt.Fprintf(os.Stderr, "skipping test: %s builder is very slow\n", b)
+	case "dragonfly-amd64":
+		// As of 2021-11-02, this builder is running with GO_TEST_TIMEOUT_SCALE=2,
+		// and seems to have unusually slow disk performance.
+		fmt.Fprintln(os.Stderr, "skipping test: dragonfly-amd64 has slow disk (https://golang.org/issue/45216)")
+	case "linux-riscv64-unmatched":
+		// As of 2021-11-03, this builder is empirically not fast enough to run
+		// gopls tests. Ideally we should make the tests faster in short mode
+		// and/or fix them to not assume arbitrary deadlines.
+		// For now, we'll skip them instead.
+		fmt.Fprintf(os.Stderr, "skipping test: %s builder is too slow (https://golang.org/issue/49321)\n", b)
+	default:
+		return
 	}
+	os.Exit(0)
 }
 
 // Go1Point returns the x in Go 1.x.
@@ -270,22 +301,92 @@ func Go1Point() int {
 
 // NeedsGo1Point skips t if the Go version used to run the test is older than
 // 1.x.
-func NeedsGo1Point(t Testing, x int) {
-	if t, ok := t.(helperer); ok {
-		t.Helper()
-	}
+func NeedsGo1Point(t testing.TB, x int) {
 	if Go1Point() < x {
+		t.Helper()
 		t.Skipf("running Go version %q is version 1.%d, older than required 1.%d", runtime.Version(), Go1Point(), x)
 	}
 }
 
 // SkipAfterGo1Point skips t if the Go version used to run the test is newer than
 // 1.x.
-func SkipAfterGo1Point(t Testing, x int) {
-	if t, ok := t.(helperer); ok {
-		t.Helper()
-	}
+func SkipAfterGo1Point(t testing.TB, x int) {
 	if Go1Point() > x {
+		t.Helper()
 		t.Skipf("running Go version %q is version 1.%d, newer than maximum 1.%d", runtime.Version(), Go1Point(), x)
 	}
+}
+
+// Deadline returns the deadline of t, if known,
+// using the Deadline method added in Go 1.15.
+func Deadline(t testing.TB) (time.Time, bool) {
+	td, ok := t.(interface {
+		Deadline() (time.Time, bool)
+	})
+	if !ok {
+		return time.Time{}, false
+	}
+	return td.Deadline()
+}
+
+// WriteImportcfg writes an importcfg file used by the compiler or linker to
+// dstPath containing entries for the packages in std and cmd in addition
+// to the package to package file mappings in additionalPackageFiles.
+func WriteImportcfg(t testing.TB, dstPath string, additionalPackageFiles map[string]string) {
+	importcfg, err := goroot.Importcfg()
+	for k, v := range additionalPackageFiles {
+		importcfg += fmt.Sprintf("\npackagefile %s=%s", k, v)
+	}
+	if err != nil {
+		t.Fatalf("preparing the importcfg failed: %s", err)
+	}
+	ioutil.WriteFile(dstPath, []byte(importcfg), 0655)
+	if err != nil {
+		t.Fatalf("writing the importcfg failed: %s", err)
+	}
+}
+
+var (
+	gorootOnce sync.Once
+	gorootPath string
+	gorootErr  error
+)
+
+func findGOROOT() (string, error) {
+	gorootOnce.Do(func() {
+		gorootPath = runtime.GOROOT()
+		if gorootPath != "" {
+			// If runtime.GOROOT() is non-empty, assume that it is valid. (It might
+			// not be: for example, the user may have explicitly set GOROOT
+			// to the wrong directory.)
+			return
+		}
+
+		cmd := exec.Command("go", "env", "GOROOT")
+		out, err := cmd.Output()
+		if err != nil {
+			gorootErr = fmt.Errorf("%v: %v", cmd, err)
+		}
+		gorootPath = strings.TrimSpace(string(out))
+	})
+
+	return gorootPath, gorootErr
+}
+
+// GOROOT reports the path to the directory containing the root of the Go
+// project source tree. This is normally equivalent to runtime.GOROOT, but
+// works even if the test binary was built with -trimpath.
+//
+// If GOROOT cannot be found, GOROOT skips t if t is non-nil,
+// or panics otherwise.
+func GOROOT(t testing.TB) string {
+	path, err := findGOROOT()
+	if err != nil {
+		if t == nil {
+			panic(err)
+		}
+		t.Helper()
+		t.Skip(err)
+	}
+	return path
 }
